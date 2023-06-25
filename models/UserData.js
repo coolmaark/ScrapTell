bcb095a9793207e32957430a0a5da927f7337923
coolmaark <ravikurella1@gmail.com>
2023-06-25 22:28:35 +0530

    MongoDB ATLAS

--- models/UserData.js
+++ models/UserData.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-mongoose.connect("mongodb://127.0.0.1:27017/UserData",function(err){
+mongoose.connect("mongodb+srv://coolmaark:Ynbf9984@scraptell.odty81u.mongodb.net/?retryWrites=true&w=majority",function(err){
     if(err){
         console.log("Database Not Connected");
     }
