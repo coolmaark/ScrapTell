feaa2da1df4f15dbcf098db2ce835f7cc7a5e07a
coolmaark <ravikurella1@gmail.com>
2023-07-07 20:23:03 +0530

    error messages in the login form done

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,27 +3,94 @@ const bcryptjs = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = require("../models/UserData");
 var jwt = require('jsonwebtoken');
+// const fetch = require('node-fetch');
+// const {axios } = require('axios');
+const { body , validationResult } = require('express-validator');
 const route = express.Router();
 
-route.post("/Login",async(req,res) =>{
-    console.log(req.body);
+
+const axios = require('axios');
+
+
+const errors = []
+
+const login = async (email, password) => {
+  try {
+    const response = await axios.post('http://localhost:3000/api/Authenticate', {
+      email,
+      password
+    });
+
+    if (response.status === 200) {
+      const token = response.data.authtoken;
+      return token;
+    } else {
+        errors.push('Login failed');
+    }
+  } catch (error) {
+    errors.push('Login failed');
+  }
+};
+
+
+const getUser = async (token) => {
+  try {
+    const response = await axios.get('http://localhost:3000/api/GetUser', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    });
+
+    if (response.status === 200) {
+      const user = response.data.user;
+      return user;
+    } else {
+        errors.push('Failed to retrieve user');
+    }
+  } catch (error) {
+    errors.push('Failed to retrieve user');
+  }
+};
+
+  
+
+
+route.post("/Login",[
+    body('email','Enter a valid Email').isEmail(),
+    body('password','Password must be atleast 5 characters').isLength({ min : 5 }),
+] ,async(req,res) =>{
+    // console.log(req.body);
     let { email , password } = req.body;
+    // let data1 = await getUser(data);
+    // if(!errors.isEmpty()){
+    //     console.log(errors);
+    // }
+    const err = validationResult(req);
+    if(!err.isEmpty()){
+        return res.render("login",{errors : err.array() });
+    }
+    let token = await login(email, password);
+    if(!token){
+        res.render("login",{error : "Please Authenticate using a valid user"});
+    }
     try{
-        let user = await User.findOne({email});
-        if(!user){
-            return res.status(400).json({error : "User does not exist"});
-        }
-
-        const check = await bcryptjs.compare(password,user.password);
-
+        const data = jwt.verify(token,process.env.JWT_STRING);
+        const user = await User.findById(data.user.id);
+        console.log(user);
+        const check = bcryptjs.compare(password,user.password);
         if(!check){
-            return res.status(400).json({error : "User does not exist"});
+            return res.render("login",{errors : "The Password is incorrect"});
         }
+
         res.redirect("/");
+
+
+    }catch(err){
+        console.log(err.message);
+        res.status(500).send("Some Error occured");
     }
-    catch(err){
-        console.log(err);
-    }
+
+   
 });
 
 route.post("/Signup",async(req,res) =>{
@@ -41,5 +108,20 @@ route.post("/Signup",async(req,res) =>{
     NewId.save();
     res.redirect("/Login");
 });
+
+// const handleSubmit = async(e)=>{
+//     e.preventionDefault();
+//     const response = await fetch("http://localhost:3000/api/Authenticate",{
+//         method = 'POST',
+//         headers : {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({email, password})
+//     });
+//     const json = await response.json();
+//     console.log(json);
+// }
+
+
 
 module.exports = route;
